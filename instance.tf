81912a86e7db38166d1e3ccbc7a318a9f4408bf5
Michel Dellabeneta <m.dellabeneta@gmail.com>
2022-12-09 20:11:04 -0300

    .

--- instance.tf
+++ instance.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "server" {
-  ami                         = "ami-0574da719dca65348"
+  ami                         = "ami-08e637cea2f053dfa"
   instance_type               = "t2.micro"
   key_name                    = "basic_infra"
   subnet_id                   = aws_subnet.public_subnet.id
-  private_ip = "10.0.1.100"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.security_group_ec2.id]
 
